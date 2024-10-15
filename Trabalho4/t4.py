74615840c58dce5e1c7f4558766ec8c369f7b12c
dreyrobert <dreyrobert@gmail.com>
2024-10-15 23:26:39 +0000

    transforma grafo em lista de adjacencia e adiciona função de gerar clique

--- Trabalho4/t4.py
+++ Trabalho4/t4.py
@@ -1,4 +1,9 @@
 from t4_aux import *
 
-grafo = le_grafo('Trabalho4/instancias_conjunto_independente_maximo/1_hamming6-2.clq')
-grafo.print()
+grafo = le_grafo('Trabalho4/instancias_conjunto_independente_maximo/2_johnson8-4-4.clq')
+## grafo.print()
+clique = GeraClique(grafo, 1)
+print(clique)
+
+
+
--- Trabalho4/t4_aux.py
+++ Trabalho4/t4_aux.py
@@ -1,40 +1,57 @@
-
-class Grafo():
-
+class Grafo:
     def __init__(self):
+        self.adjacencia = {}
         self.vertices = 0
-        self.arestas = []
 
     def set_vertices(self, vertices):
         self.vertices = vertices
+        self.adjacencia = {i: set() for i in range(1, vertices + 1)}
 
     def add_aresta(self, aresta):
-        self.arestas.append(aresta)
+        u, v = aresta
+        self.adjacencia[u].add(v)
+        self.adjacencia[v].add(u)
 
     def print(self):
-        print(self.vertices)
-        for aresta in self.arestas:
-            print(f'[{aresta[0]} - {aresta[1]}]')
+        print(f"Número de vértices: {self.vertices}")
+        print("Lista de adjacência:")
+        for vertice, vizinhos in self.adjacencia.items():
+            print(f"{vertice}: {sorted(vizinhos)}")
 
 def le_grafo(path):
-    
     with open(path, 'r') as f:
-        
         g = Grafo()
-
         linhas = f.readlines()
         for linha in linhas:
-            
             l = linha.split(" ")
             match l[0]:
                 case 'c':
                     pass
                 case 'p':
                     g.set_vertices(int(l[2]))
-                    pass
                 case 'e':
                     g.add_aresta((int(l[1]), int(l[2])))
-                    pass
                 case _:
                     pass
-    return g         
+    return g
+
+def GeraClique(grafo, v):
+    adjacencia = grafo.adjacencia
+
+    Q = {v}.union(adjacencia[v])
+
+    for u in list(Q):
+        if u == v:
+            continue
+        
+        vizinhos = False
+        for w in Q:
+            if w != u:  
+                if w not in adjacencia[u]:  
+                    vizinhos = True
+                    break
+        
+        if vizinhos:
+            Q.remove(u)
+
+    return Q
